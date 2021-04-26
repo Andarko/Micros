42f8258a2e498119787b24af169f72385ed5fc03
Andrey <Andarko@bk.ru>
2021-04-26 16:34:16 +0400

    Мой модуль сравнения двух изображений

--- modules_sersh4nt/image_difference/difference_my.py
+++ modules_sersh4nt/image_difference/difference_my.py
@@ -1,6 +1,11 @@
+import random
+
 import cv2
 import colorsys
 import os
+
+import numpy as np
+from thop.vision.basic_hooks import count_convNd_ver2
 
 
 def get_colours():
@@ -16,48 +21,126 @@ def get_colours():
 
 
 def get_difference(img1, img2, colours):
-    img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-    res = img1_gray - img2_gray
-    result = img1.copy()
-    for x in range(result.shape[0]):
-        for y in range(result.shape[1]):
-            result[x][y] = colours[res[x][y]]
-    return result
+    # img1.astype(np.int)
+    # img2.astype(np.int)
+    h, w, rate = img1.shape
+    # Преобразование к типу int для возможности вычитания
+    img1 = np.array(img1, dtype='int')
+    img2 = np.array(img2, dtype='int')
+    # result = np.zeros((img1.shape[0], img1.shape[1], img1.shape[2]), 'int')
+    # result = np.copy(img1)
+    # img2_int = np.zeros((img2.shape[0], img2.shape[1], img2.shape[2]), 'int')
+    # img2_int = np.copy(img2)
+    # result -= img2_int
+    # Берем абсолютные цифры
+    absolute = np.abs(img1 - img2)
+    # Складываем цифры по трем цветам в единую сущность
+    result = absolute[:, :, 0:1] + absolute[:, :, 1:2] + absolute[:, :, 2:3]
+    # Применим теперь нормирующую функцию с заданным коэффициентом чувствительности
+    # Из-за сложения трех цветов - начальное значение может быть от 0 до 3 * 256 = 768
+    # Минимальный порог чувствительности - все, что ниже него не отображаем
+    sensitivity_min = 24
+    # Остальное красим от 0 до 255, данный коэффициент насыщает краски
+    sensitivity_rate = 5
+    result2 = result - sensitivity_min
+    result2[result2 < 0] = 0
+    result3 = result2 * sensitivity_rate
+    result3[result3 > 255] = 255
+    result4_color = np.array(result3, dtype='uint8')
+    result4_zero = np.zeros(shape=result4_color.shape, dtype=result4_color.dtype)
+    result4 = np.concatenate((result4_zero, result4_color, result4_color), axis=2)
+    print(np.mean(result))
+    print(np.max(result))
+    print(np.min(result))
+
+    return result4
 
 
-def crop_image(w, h, img):
+def crop_image(h, w, img):
     res = img.copy()
     res = res[
-          (res.shape[0] - w) // 2: (res.shape[0] + w) // 2,
-          (res.shape[1] - h) // 2: (res.shape[1] + h) // 2
+          (res.shape[0] - h) // 2: (res.shape[0] + h) // 2,
+          (res.shape[1] - w) // 2: (res.shape[1] + w) // 2
     ]
     return res
 
 
 def get_difference_images(img1, img2, colours):
-    w, h, _ = img1.shape
-    w = min(w, img2.shape[0])
-    h = min(h, img2.shape[1])
-    crop1 = crop_image(w, h, img1)
-    crop2 = crop_image(w, h, img2)
-    result = get_difference(crop1, crop2, colours)
-    return cv2.addWeighted(crop1, 0.5, result, 0.5, 0)
+    # Обрезка по меньшему размеру
+    h, w, _ = img1.shape
+    h = min(h, img2.shape[0])
+    w = min(w, img2.shape[1])
+    crop1 = crop_image(h, w, img1)
+    crop2 = crop_image(h, w, img2)
+    blur = []
+    blur = [cv2.blur(src=img1, ksize=(3, 3), dst=0), cv2.blur(src=img2, ksize=(3, 3), dst=0)]
+    # Функция сравнения
+    difference = get_difference(blur[0], blur[1], colours)
+    orig_rate = 80
+    dif_rate = 80
+    return cv2.addWeighted(crop1, orig_rate / 100.0, difference, dif_rate / 100.0, 0.0)
 
 
-# img1 = cv2.imread("1sd.jpg")
-# img2 = cv2.imread("2sd.jpg")
+# Загружаем картинки для сравнения
 img = []
-
 for root, dirs, files in os.walk("IMG"):
+    files.sort()
     for file in files:
         if len(img) < 2:
             if file and file.endswith(".jpg"):
-                img.append(os.path.join("IMG", file))
+                img.append(cv2.imread(os.path.join("IMG", file)))
+# Размываем немного картинки для того, чтобы избавиться от шума при сравнении
 
 
-res = get_difference_images(cv2.imread(img[0]), cv2.imread(img[1]), get_colours())
-cv2.imwrite(os.path.join("IMG", "Z_gray_res.jpg"), res)
+res = get_difference_images(img[0], img[1], get_colours())
+cv2.imwrite(os.path.join("Z_gray_res.jpg"), res)
 cv2.imshow("result", res)
 cv2.waitKey(0)
+#
+# a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+#               15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]).reshape(4, 4, 2)
+# a = np.abs(a)
+# print(a)
+# print(a[:, :, 1:2])
+# b = np.roll(a, -1, axis=0)[:2, :]
+# print(b)
+# a[:2, :] += b
+# print(a)
+
+#
+# def delete_me_1():
+#     rost = []
+#     for i in range(20):
+#         rost.append(random.randint(0, 50))
+#
+#     knowing_count = []
+#     for i in range(len(rost)):
+#         count = 1
+#         next_man_index = i
+#         # Цикл передачи к следующему
+#         while True:
+#             receiver = next_man_index
+#             receiver_rost = 10000000
+#             # Цикл поиска следующего
+#             for j in range(next_man_index + 1, len(rost)):
+#                 if receiver_rost > rost[j] > rost[next_man_index]:
+#                     receiver = j
+#                     receiver_rost = rost[j]
+#             if receiver > next_man_index:
+#                 next_man_index = receiver
+#                 count += 1
+#             else:
+#                 break
+#         knowing_count.append(count)
+#
+#     print(rost)
+#     print(knowing_count)
+#
+
+
+
+
+
+
+
 
